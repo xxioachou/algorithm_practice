28a608d85a2575dd12c3ffc100d96d67f2ea584e
hmc <2273691738@qq.com>
2025-03-18 15:29:04 +0800

    2025/3/18

--- cpp_code/transfer/baoli.cpp
+++ cpp_code/transfer/baoli.cpp
@@ -4,28 +4,29 @@ using namespace std;
 void solve() {
     int n;
     cin >> n;
-    deque<pair<int, int>> q;
     int ans = 0;
-    while (n -- > 0) {
-        int op, price, t;
-        cin >> op >> price >> t;
+    vector<int> op(n), price(n), t(n);
+    for (int i = 0; i < n; i ++) {
+        cin >> op[i] >> price[i] >> t[i];
+    }
 
-        if (!op) {
-            ans += price;
-            q.emplace_back(t, price);
+    // 存储所有可用的优惠
+    vector<int> idx, vis(n);
+    for (int i = 0; i < n; i ++) {
+        if (!op[i]) {
+            ans += price[i];
+            idx.push_back(i);
         } else {
-            while (!q.empty() && t - q.front().first > 45) {
-                q.pop_front();
-            }
             bool ok = false;
-            for (auto it = q.begin(); it != q.end(); it ++) {
-                if (it->second >= price) {
+            for (int j : idx) {
+                if (price[j] >= price[i] && !vis[j] && t[i] - t[j] <= 45) {
+                    vis[j] = 1;
                     ok = true;
                     break;
                 }
             }
             if (!ok) {
-                ans += price;
+                ans += price[i];
             }
         }
     }
